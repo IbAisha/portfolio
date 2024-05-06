715ed5590364150bd1888727177a3846acde4680
Aisha <zawjatu.a@gmail.com>
2024-05-06 16:07:17 +0200

    Add bg + nav

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -5,7 +5,13 @@ function Nav() {
     return (
         <nav className="navbar">
             <div className="navbar-container">
-                <img src="src/assets/img/logo.png" alt="logo Ibrahima Aicha" width="120px" />
+                <img src="src/assets/img/logo.png" alt="logo Ibrahima Aicha" width="200px" />
+
+                {/* <ul>
+                    <li>Home</li>
+                    <li>Projets</li>
+                    <li>About me ?</li>
+                </ul> */}
             </div>
         </nav>
     )
